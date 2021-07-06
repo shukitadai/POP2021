594b979ae8d4462ed9364e096afac3913cbe249d
shukitadai <84366171+shukitadai@users.noreply.github.com>
2021-07-06 14:34:19 +0900

    Add files via upload

---
+++ Loops practice.py
@@ -0,0 +1,27 @@
+#answer = 0
+#for x in range(1,101):
+#   answer += x
+
+#print(answer)
+
+def add_10():
+    answer = 0
+    for x in range(1,11):
+        answer += x
+    print(answer)
+
+add_10()
+
+def add_n(n):
+    answer = 0
+    for x in range(1,n+1):
+        answer += x
+    print(answer)
+
+add_n(2021)
+
+my_list = [56,37,82,2340,5]
+answer =0
+for x in my_list:
+    answer += x
+print(answer)
