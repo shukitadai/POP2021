a4eaf7b43aa974c258518f3bfb04ae9e4987d1a7
shukitadai <84366171+shukitadai@users.noreply.github.com>
2021-07-13 14:11:45 +0900

    Add files via upload

--- manga_ka.py
+++ manga_ka.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Manga_ka:
     def __init__(self):
         self.name = ""
@@ -7,14 +9,22 @@ class Manga_ka:
         self.date_of_birht = \
                     datetime.date.today()
         self.manga_list = []
-        self.genre = ""
+        self.genre = "\n"
+    def __str__(self):
+        mangas = "\n"
+        for x in self.manga_list:
+            mangas += x + "\n"
+        return self.name + "\n" +self.publisher \
+               + mangas
 
         
 
 ken_wakui = Manga_ka()
 ken_wakui.manga_list.append("Tokyo Revengers")
-ken_wakui.name = "Wakui  ken"
+ken_wakui.name = "Wakui ken"
 ken_wakui.country = "Japan"
 ken_wakui.publisher = "Magajin"
-ken_wakui.gender = ""
-print(oda_eichiro.manga_list)
+ken_wakui.gender = "Male"
+ken_wakui.genre = "Syounenn Manga"
+
+print(ken_wakui.manga_list)
