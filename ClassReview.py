21099bb4c4a1866960a335e3c8c1015b61c37025
shukitadai <84366171+shukitadai@users.noreply.github.com>
2021-11-30 15:01:06 +0900

    Add files via upload

---
+++ ClassReview.py
@@ -0,0 +1,58 @@
+class Cat:
+    def __init__(self,co,na,dr,ge,ag):
+        self.color = co
+        self.name = na
+        self.dreed = dr
+        self.gender = ge
+        self.age = ag
+        self.xpos = 0
+        self.ypos = 0
+    def jump(self.height):
+        print("Nya")
+        
+       
+        
+    
+
+class Cattwo:
+    def __init__(self):
+        self.color = ""
+        self.name = ""
+        self.dreed = ""
+        self.gender = ""
+        self.age = 0
+        self.xpos = 0
+        self.ypos = 0
+
+class Catthree:
+    def __init__(self,no = "",na = "",dr = "",ge = "",ag = 0):
+        self.color = no
+        self.name = na
+        self.dreed = dr
+        self.gender = ge
+        self.age = ag
+        self.xpos = 0
+        self.ypos = 0
+
+
+cat = Cat("Tama","F","White","Mike",0)
+
+cat2 = Cattwo()
+cat2.name = "Tama"
+cat2.dreed = ""
+cat2.gender = ""
+cat2.age = 0
+
+
+
+class dog:
+    def __init__(self,co,na,br,ge,ag):
+        self.color = co
+        self.name = na
+        self.dreed = dr
+        self.gender = ge
+        self.age = ag    
+
+
+
+    
---
+++ Functions Review.py
@@ -0,0 +1,5 @@
+s = input("")
+
+print(s.count("a"))
+
+print(s.count("apple"))
