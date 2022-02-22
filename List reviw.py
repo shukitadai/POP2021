a38ccd0341a1a753f3c049666e7d6700ef3f108b
shukitadai <84366171+shukitadai@users.noreply.github.com>
2022-02-22 14:24:45 +0900

    Add files via upload

---
+++ List reviw.py
@@ -0,0 +1,35 @@
+education = ["Nursery School","Elementary School",\
+             "Junior High School","High school",\
+             "Vocational School",\
+             "College/University","Graducate School"]
+
+#print(len(education))
+
+#print(education[2])
+
+#print(education[0])
+
+#print(education[6])
+
+#print(education[len(education)-1])
+
+#print(education[-4])
+
+#for x in education:
+    #print(x)
+
+
+#for x in range(len)education)):
+    #print(education[x])
+
+current_education = 3
+print(education[current_education])
+
+
+if current_education + 1 <= len(education):
+current_education = current_education + 1
+
+#if current_education + 1 >= len(education):
+    return
+
+print(education[current_education])
